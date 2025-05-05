dfa05e8bca58189c483f2face3ce8dc278607ef8
marcmarant <marcosmardev@gmail.com>
2025-05-06 01:45:20 +0200

    change image width

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ export const NavBar = ({ children }: { children: ReactNode }) => {
   return (
     <nav className="fixed z-50 w-full px-8 py-6 flex items-center bg-background shadow-sm">
       <Link href="/" className="absolute">
-        <Image src="/images/logo.png" alt="Logo" width={30} height={30} />
+        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
       </Link>
       {children}
     </nav>
