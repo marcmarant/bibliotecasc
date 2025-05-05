4592c4c127c7361287fcc6dd60f4c3f47e0513b9
marcmarant <marcosmardev@gmail.com>
2025-05-06 01:33:33 +0200

    revert add types

--- src/components/BookGrid.tsx
+++ src/components/BookGrid.tsx
@@ -3,7 +3,7 @@
 import { Card } from '@/components/ui/card'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Book } from '@/types/prisma'
+import { Book } from '@prisma/client'
 
 interface BookGridProps {
   books: Book[]
